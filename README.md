fbd4b79edb52348455bcb5f0a534873c251f201b
Zachary Barnes <zachhsavage@gmail.com>
2023-04-13 18:04:10 -0400

    Finished adding the additional functions the project needed to be completed. Also finished the README.

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# Employee_Tracker
+# **Employee Tracker**
+
+  ## **Description**
+  - Using inquirer, you can create a table that keeps track of your employee information. 
+
+  - I learned so much more with inquirer, and mySQL.
+
+**Repository URL:** https://github.com/Animeet/Employee_Tracker
+
+**Video Demonstration URL:** https://www.youtube.com/watch?v=KgS2bjlOWXs
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [MIT](https://opensource.org/licenses/MIT)
+  - [Questions](#questions)
+
+  ---
+
+  ## **Installation**
+  To install, please download the folder/repository through the github url above.
+
+  ---
+
+  ## **Usage Information**
+  You'll be using this application directly through your terminal. 
+  To get started, simple type in 'node index.js'
+  You'll then be prompted with a list of choices within the terminal.
+  Once you have followed all of the prompts, and are happy with the current state of your employee information, type in 'CTRL + C' to exit out of the database.
+
+
+  ---
+  
+  ## **Contribution**
+  This is an open source project. If you'd like to contribute, simply create a pull request.
+
+
+  ---
+
+  ## **Testing**
+  Within the terminal type in 'node index.js'.
+
+
+  ---
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+   ## **License**
+  License: MIT
+
+  For more information on the license above, please go to [https://www.choosealicense.com/](https://www.choosealicense.com/).
+
+  ---
+
+  ## **Questions**
+  If you have any questions about this repository, please contact me directly at [zachhsavage@gmail.com](mailto:zachhsavage@gmail.com).
+  You can find this repository, and others I have worked on at [Animeet](https://www.github.com/Animeet).
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const Queries = require('./queries/queries.js');
 require("console.table")
 
 const connection = mysql.createConnection({
@@ -28,76 +27,226 @@ const questions = [
     }
 ];
 
-function viewAllEmployees () {
-    connection.query(`SELECT e1.first_name, e1.last_name, title, salary, name FROM employee e1 
+
+
+/* * * View All Employees * * */
+function viewAllEmployees() {
+    connection.query(`SELECT e1.id, e1.first_name, e1.last_name, title, salary, name AS department, CONCAT(e2.first_name, ' ', e2.last_name) AS manager FROM employee e1 
     JOIN role ON e1.role_id = role.id 
     JOIN department ON role.department_id = department.id 
     LEFT JOIN employee e2 ON e1.manager_id = e2.id`, function (err, results, fields) {
+        if (err) {
+            console.log(err);
+            return;
+        }
         console.table(results);
         init();
     });
 }
 
-function addEmployee () {
-    connection.query(`SELECT * FROM role`, function (err, results) {
-        console.log(results)
 
-        const roleChoices = results.map(function(role){
+
+/* * * Add Employee * * */
+function addEmployee() {
+    connection.query(`SELECT * FROM role`, function (err, results) {
+
+        const roleChoices = results.map(function (role) {
             return {
                 name: role.title,
                 value: role.id
             };
         })
-    connection.query(`SELECT * FROM employee`, function (err, results) {
-        console.log(results);
 
-        const managerChoice = results.map(function(manager){
+        connection.query(`SELECT * FROM employee`, function (err, results) {
+
+            const managerChoice = results.map(function (manager) {
+                return {
+                    name: manager.first_name + ' ' + manager.last_name,
+                    value: manager.id
+                }
+            })
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is the employee's first name?",
+                    name: "f_name"
+                },
+                {
+                    type: "input",
+                    message: "What is the employee's last name?",
+                    name: "l_name"
+                },
+                {
+                    type: "list",
+                    message: "What is the employee's role?",
+                    name: "role_id",
+                    choices: roleChoices
+                },
+                {
+                    type: "list",
+                    message: "Who is the employee's manager?",
+                    name: "manager_id",
+                    choices: managerChoice
+                }
+            ]).then(function (data) {
+                connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES ("${data.f_name}", "${data.l_name}", ${data.role_id}, ${data.manager_id})`, function (err, results, fields) {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+
+                    init();
+                });
+            })
+        })
+    })
+}
+
+
+
+/* * * Update Employee * * */
+function updateEmployee() {
+    connection.query(`SELECT * FROM role`, function (err, results) {
+
+        const roleChoices = results.map(function (role) {
             return {
-                name: manager.first_name + ' ' + manager.last_name,
-                value: manager.id
-            }
+                name: role.title,
+                value: role.id
+            };
+        })
+
+        connection.query(`SELECT * FROM employee`, function (err, results) {
+
+            const employeeChoices = results.map(function (employee) {
+                return {
+                    name: employee.first_name + ' ' + employee.last_name,
+                    value: employee.id
+                }
+            })
+            inquirer.prompt([
+                {
+                    type: "list",
+                    message: "Which employee's role do you want to update?",
+                    name: "employee_id",
+                    choices: employeeChoices
+                },
+                {
+                    type: "list",
+                    message: "Which role do you want to assign the selected employee?",
+                    name: "role_id",
+                    choices: roleChoices
+                }
+            ]).then(function (data) {
+                connection.query(`UPDATE employee SET role_id = ${data.role_id} WHERE id = ${data.employee_id}`, function (err, results, fields) {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+
+                    init();
+                });
+            })
+        })
+    })
+};
+
+
+
+/* * * View All Roles * * */
+function viewAllRoles() {
+    connection.query(`SELECT role.id, title, name AS department, salary FROM role 
+    JOIN department ON role.department_id = department.id `, function (err, results, fields) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(results);
+        // console.table(results);
+        init();
+    });
+}
+
+
+
+/* * * Add Roles * * */
+function addRole() {
+    connection.query(`SELECT * FROM department`, function (err, results) {
+
+        const departmentChoices = results.map(function (department) {
+            return {
+                name: department.name,
+                value: department.id
+            };
         })
         inquirer.prompt([
             {
                 type: "input",
-                message: "What is the employee's first name?",
-                name: "f_name"
+                message: "What is the name of the role?",
+                name: "role_name"
             },
             {
-                type: "input",
-                message: "What is the employee's last name?",
-                name: "l_name"
-            },
-            {
-
-                type: "list",
-                message: "What is the employee's role?",
-                name: "role_id",
-                choices: roleChoices
+                type: "number",
+                message: "What is the salary of the role?",
+                name: "salary"
             },
             {
                 type: "list",
-                message: "Who is the employee's manager?",
-                name: "manager_id",
-                choices: managerChoice
+                message: "Which department does the role belong to?",
+                name: "department",
+                choices: departmentChoices
             }
         ]).then(function (data) {
-            connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
-            VALUES ("${data.f_name}", "${data.l_name}", ${data.role_id}, ${data.manager_id})`, function (err, results, fields) {
-               if(err) {
-                console.log(err);
-                return;
-               }
-               console.log(results);
-               init();
+            connection.query(`INSERT INTO role (title, salary, department_id)
+        VALUES ("${data.role_name}", "${data.salary}", "${data.department}")`, function (err, results, fields) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+
+                init();
             });
         })
     })
+}
 
-        // const roleChoices = results;
-        
+
+
+/* * * View All Departments * * */
+function viewAllDepartments() {
+    connection.query(`SELECT id, name FROM department`, function (err, results, fields) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(results);
+        init();
+    });
+}
+
+
+
+/* * * Add Department * * */
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of the department?",
+            name: "department_name"
+        },
+    ]).then(function (data) {
+        connection.query(`INSERT INTO department (name)
+        VALUES ("${data.department_name}")`, function (err, results, fields) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            init();
+        });
     })
 }
+
 
 
 // TODO: Create a function to initialize app
@@ -106,9 +255,24 @@ function init() {
     inquirer.prompt(questions).then(function (data) {
         if (data.choices === "View All Employees") {
             viewAllEmployees()
-        };
-        if (data.choices === "Add Employee") {
+        }
+        else if (data.choices === "Add Employee") {
             addEmployee()
+        }
+        else if (data.choices === "Update Employee Role") {
+            updateEmployee()
+        }
+        else if (data.choices === "View All Roles") {
+            viewAllRoles()
+        }
+        else if (data.choices === "Add Role") {
+            addRole()
+        }
+        else if (data.choices === "View All Departments") {
+            viewAllDepartments();
+        }
+        else if (data.choices === "Add Department") {
+            addDepartment();
         }
         // init();
     })
